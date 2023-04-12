56aada691189cfbb6440b55444da1862d564483a
HoaiNam2312 <namhoai2312@gmail.com>
2023-04-12 21:23:49 +0700

    add

---
+++ ss3_Angular_Component_Template/Exercise/angular-hackernews-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Article} from './model/article';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  article: Article = {
+    title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
+    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+  };
+  title: Article;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
